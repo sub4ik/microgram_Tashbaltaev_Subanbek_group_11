dbbe223e4663888b96d39facc559fa13f40bb538
Subanbek <stashbaltaev@gmail.com>
2022-04-29 20:39:05 +0600

    finished HW 63 ez

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -2,37 +2,14 @@
 // document.querySelector('.login-wrap').hidden = true
 const formSignIn = document.querySelector('#sign_in')
 const formSignUp = document.querySelector('#sign_up')
+const form = document.querySelector('.login-wrap')
+const page = document.querySelector('.page')
 
 if (localStorage.getItem('username') != null){
 
-    document.querySelector('.login-wrap').hidden = true
-    document.querySelector('.page').hidden = false
+    formHidden(true)
 } else {
-    document.querySelector('.login-wrap').hidden = false
-    document.querySelector('.page').hidden = true
-}
-
-async function authUser(userFormData) {
-    const a = JSON.stringify(Object.fromEntries(userFormData));
-    console.log(a)
-    const user = {
-        username : userFormData.get("accountName"),
-        password : userFormData.get("password")
-    }
-    console.log(user)
-    const baseUrl = 'http://localhost:8081/users/login';
-    const settings = {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Basic ' + window.btoa(user.username + ':' + user.password)
-        }
-    };
-    // console.log('qweqwewq')
-    const response = await fetch(baseUrl + `?text=${userFormData.get("accountName")}`, settings)
-    // .then(return response.json())
-    console.log(response)
-    
+    formHidden(false)
 }
 
 formSignIn.addEventListener('submit', e => {
@@ -43,28 +20,84 @@ formSignIn.addEventListener('submit', e => {
     authUser(userFormData);
 })
 
+formSignUp.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const userFormData = new FormData(e.target)
+    console.log(userFormData.get("username"))
+    createUser(userFormData);
+})
+
+
+page.addEventListener('click', () => {
+    console.log("vyhoju")
+    localStorage.clear()
+    formHidden(false)
+})
+
+
+async function authUser(userFormData) {
+    const a = JSON.stringify(Object.fromEntries(userFormData));
+    console.log(a)
+    const user = {
+        username : userFormData.get("accountName"),
+        password : userFormData.get("password")
+    }
+    const baseUrl = 'http://localhost:8081/users/login';
+    const settings = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + window.btoa(user.username + ':' + user.password)
+        }
+    };
+    const response = await fetch(baseUrl + `?text=${userFormData.get("accountName")}`, settings)
+    .then (response=>{
+        
+        console.log(response)
+        if(response.ok){
+            localStorage.setItem('username', JSON.stringify(user))
+            formHidden(true)
+        } else alert("email или пароль не верны");
+
+    })
+    
+}
+
+
+
 // ------------------------------------------------------------
 // document.querySelector('.login-wrap').hidden = true
 
-// async function createUser(userFormData) {
-//     const a = JSON.stringify(Object.fromEntries(userFormData));
-//     console.log(a)
-//     const baseUrl = 'http://localhost:8081';
-//     const settings = {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: a
-//     };
-//     // console.log('qweqwewq')
-//     const response = await fetch(baseUrl + '/users/registration', settings);
-// }
+async function createUser(userFormData) {
+    const a = JSON.stringify(Object.fromEntries(userFormData));
+    console.log(a)
+    const baseUrl = 'http://localhost:8081';
+    const settings = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: a
+    };
+    // console.log('qweqwewq')
+    const user = {
+        username : userFormData.get("accountName"),
+        password : userFormData.get("password")
+    }
+    const response = await fetch(baseUrl + '/users/registration', settings)
+    .then(response=>{
+        console.log(response)
+        if(response.ok){
+            localStorage.setItem('username', JSON.stringify(user))
+            formHidden(true)
+        } else alert("Это имя или почта уже занато, введи другие данные");
+    })
 
-// formSignUp.addEventListener('submit', e => {
-//     e.preventDefault()
+}
+function formHidden(bool){
+    form.hidden = bool;
+    page.hidden = !bool;
+}
 
-//     const userFormData = new FormData(e.target)
-//     console.log(userFormData.get("username"))
-//     createUser(userFormData);
-// })
+
